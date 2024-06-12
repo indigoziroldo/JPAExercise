9e1dfad44aeddd2d730a156ec19b0f7a7d74faa6
Indigo Ziroldo <126610921+indigoziroldo@users.noreply.github.com>
2024-06-12 15:08:58 -0300

    Delete src directory

--- src/main/java/br/edu/fema/modelo/atividadesfixacao/domain/alimento/repository/TipoAlimentoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.fema.modelo.atividadesfixacao.domain.alimento.repository;
-
-import br.edu.fema.modelo.atividadesfixacao.domain.alimento.entity.TipoAlimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoAlimentoRepository extends JpaRepository<TipoAlimento, Long> {
-
-}
--- src/main/java/br/edu/fema/modelo/atividadesfixacao/utils/conversao/enums/interfaces/GettersEnum.java
+++
@@ -1,6 +0,0 @@
-package br.edu.fema.modelo.atividadesfixacao.utils.conversao.enums.interfaces;
-
-
-public interface GettersEnum<E> extends ValorEnum<E>, DescricaoEnum {
-
-}
